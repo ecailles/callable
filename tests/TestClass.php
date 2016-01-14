a3f18b9aab68ecb55159f6afbc4193d97b44307a
Whizark <contact@whizark.com>
2016-01-15 05:31:19 +0900

    test: re-order methods to be static function first

--- tests/TestClass.php
+++ tests/TestClass.php
@@ -3,21 +3,21 @@ namespace Tests\Ecailles\CallableObject;
 
 class TestClass
 {
-    public function instanceMethodWithParameters($parameter1, $parameter2)
-    {
-        return [$parameter1, $parameter2];
-    }
-
-    public function instanceMethodWithoutParameters()
-    {
-    }
-
     public static function classMethodWithParameters($parameter1, $parameter2)
     {
         return [$parameter1, $parameter2];
     }
 
     public static function classMethodWithoutParameters()
+    {
+    }
+
+    public function instanceMethodWithParameters($parameter1, $parameter2)
+    {
+        return [$parameter1, $parameter2];
+    }
+
+    public function instanceMethodWithoutParameters()
     {
     }
 }
